7b83cfcde4c419f1b331216ac4fa5545da5b2d8b
Minhee Kim <150657776+minkimNV@users.noreply.github.com>
2025-05-23 22:09:42 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,8 @@
+select
+member_id, member_name, gender, date_format(date_of_birth, '%Y-%m-%d') as date_of_birth
+
+from member_profile
+where date_format(date_of_birth, '%m') = '03'
+and gender = 'W'
+and tlno is not null
+order by member_id

5166e8a630caa1a957c92abcbf12ca2dd6394b01
Minhee Kim <150657776+minkimNV@users.noreply.github.com>
2025-05-23 21:21:17 +0900

    [level 1] Title: 역순 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59035. 역순 정렬하기/역순 정렬하기.sql
@@ -0,0 +1,35 @@
+# -- 코드를 입력하세요
+# SELECT NAME, DATETIME
+#     FROM ANIMAL_INS
+#     ORDER BY ANIMAL_ID DESC, NAME, DATETIME;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+select
+name, datetime
+from animal_ins
+order by animal_id desc
